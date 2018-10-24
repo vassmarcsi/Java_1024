aa54b13f8ce2b0c77dc976faf3a0d6c8befafb66
Maria Vass <vassmarcsi88@gmail.com>
2018-10-24 16:40:07 +0200

    Előző óra újra

---
+++ Okt24/src/application/ViewController.java
@@ -0,0 +1,35 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ViewController {
+
+    @FXML
+    private TableView<?> tabla;
+
+    @FXML
+    private TextField idMezo;
+
+    @FXML
+    private TextField VezNevMezo;
+
+    @FXML
+    private TextField KerNevMezo;
+
+    @FXML
+    private TextField SzulDatumMezo;
+
+    @FXML
+    void menuItemKilepes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void menuItemNevjegy(ActionEvent event) {
+
+    }
+
+}
